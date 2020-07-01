0c85a79557926c7a115ae833c54f109d6b0eebbd
Vu Tuan Dat <datvt18@viettel.com.vn>
2020-07-01 09:06:32 +0700

    Add dashboard

---
+++ src/components/cards/Dashboards.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container style="height:100%">
+    <iframe
+      :src="process.env.VUE_APP_FAYTHE_DASHBOARD_URL + `&theme=light&kiosk=tv`"
+      width="100%"
+      height="100%"
+      frameborder="0"
+    ></iframe>
+  </v-container>
+</template>
